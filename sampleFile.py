186d6e6c6571d2e994bdf9e28e91cd327ede15a1
Alexandre Guerville <alexandre.guerville@hotmail.com>
2021-03-14 00:58:55 +0100

    Added sample file

---
+++ sampleFile.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def direBonjour():
+    pour i dans range(1, 10):
+        si(i < 6):
+            print("Bonjour Monde!")
+        ousi( i < 8 ):
+            print("Croissant")
+        sinon:
+            print("Au revoir!")
+    retourner 5
+
+direBonjour()
