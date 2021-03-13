64df22403b63a188ccda1b91810b618f8d473a08
Tamim Azmain <maat1@st-andrews.ac.uk>
2021-03-14 00:39:46 +0900

    dummy method

--- src/main/java/Transpire/Translations.java
+++ src/main/java/Transpire/Translations.java
@@ -7,18 +7,30 @@ import java.util.*;
 
 public class Translations {
 	
-	public Translations(String languageToLoad, String rootPath) throws NotSupportedLanguage{
+	public Translations(String sLang, String pLang, String rootPath) throws NotSupportedLanguage{
 		File rootFolder = new File(rootPath);
 
-		List<File> supportedLanguages = Arrays.asList(rootFolder.listFiles());
-		boolean supportsLanguage = false;
-		for(File folder: supportedLanguages){
-			if(languageToLoad.equals(folder.getName())){
-				supportsLanguage = true;
-			}
-		}
-		if(!supportsLanguage){
-			throw new NotSupportedLanguage("Unsupported language. Saweee");
+		// List<File> supportedSLangs = Arrays.asList(rootFolder.listFiles());
+		// boolean foundSLang = false;
+		// boolean foundPLang = false;
+
+		// for(File sLangFolderName: supportedSLangs){
+		// 	if(sLangFolderName.equals(sLangFolderName.getName())){
+		// 		foundPLang = true;
+				
+		// 		File l
+		// 	}
+		// }
+		// if(!supportsLanguage){
+		// 	throw new NotSupportedLanguage("Unsupported language. Saweee");
+		// }
+	}
+
+	public String getMapping(String languageToken){
+		if(languageToken.equals("sinon")){
+			return "else";
+		}else{
+			return "if";
 		}
 	}
 }
