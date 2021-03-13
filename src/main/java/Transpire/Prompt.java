771adbb74146b70886dd4d85513c09189938abd1
Alexandre Guerville <alexandre.guerville@hotmail.com>
2021-03-13 16:09:03 +0100

    Wrote base version of argument parser.

--- src/main/java/Transpire/Prompt.java
+++ src/main/java/Transpire/Prompt.java
@@ -1,6 +1,39 @@
 package Transpire;
 
+import java.io.File;
+
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+
+
 public class Prompt
 {
-    
+	public int launchPrompt(String[] args) {
+		ArgumentParser parserEng = ArgumentParsers.newFor("transpire").build()
+			.description("Translates program written in native language to "
+						 + "compilable code.");
+		parserEng.addArgument("files")
+			.metavar("FILE")
+			.type(File.class)
+			.nargs("+")
+			.help("A file to translate to other language");
+		parserEng.addArgument("--lang", "-l")
+			.required(true)
+			.dest("programming language")
+			.type(String.class)
+			.choices("python", "c")
+			.help("The programming language used here.");
+		parserEng.addArgument("--source", "-s")
+			.required(true)
+			.dest("source language")
+			.type(String.class)
+			.choices("en", "fr")
+			.help("The source file natural language.");
+		parserEng.addArgument("--target")
+			.dest("target language")
+			.type(String.class)
+			.choices("en", "fr")
+			.help("The spoken language to translate the file to.");
+		return 0;
+	}
 }
